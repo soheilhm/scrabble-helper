9b0ea14d816e809b52378856788c421352e5e40e
soheil <soheilhajimohammadi@ip-192-168-1-68.us-west-2.compute.internal>
2020-12-23 14:42:07 -0800

    migrated to new repo

---
+++ precache-manifest.899825cf822d38fdb62d78fa5f143bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b789666a843ed5b9a34a202fe525f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54dc8f10a609f1693c56",
+    "url": "/static/css/2.6883e14e.chunk.css"
+  },
+  {
+    "revision": "d86aabcc5b9903d3e49c",
+    "url": "/static/css/main.815c384c.chunk.css"
+  },
+  {
+    "revision": "54dc8f10a609f1693c56",
+    "url": "/static/js/2.b82d3c28.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.b82d3c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86aabcc5b9903d3e49c",
+    "url": "/static/js/main.34c62b45.chunk.js"
+  },
+  {
+    "revision": "8e52442222ee26ed856a",
+    "url": "/static/js/runtime-main.8ed7f60c.js"
+  }
+]);
